5e218feb007abe5c1786d52c653562753af31241
Noah Potischmane <noahpotischman2026@kehillah.student.org>
2024-08-28 14:09:12 -0700

    made insert and insertR recursive methods

--- binarysearchtree/BinarySearchTree.java
+++ binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,30 @@
 package binarysearchtree;
 
 public class BinarySearchTree {
+    Node root;
+
+    BinarySearchTree(){
+        this.root = null;
+    }
+
+    public void insert(int data) {
+        root = insertR(root, data);
+    }
+
+    public Node insertR(Node root, int data) {
+
+        if(root == null) {
+            root = new Node(data);
+        }
+
+        // need to recur down tree
+        if (data < root.data) {
+            root.left = insertR(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertR(root.right, data);
+        }
+        return root;
+    }
+
     
 }
