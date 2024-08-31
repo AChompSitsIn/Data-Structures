d7ac871e75b6bf8f183e4a8895aa8b240f8d4f2a
Noah Potischmane <noahpotischman2026@kehillah.student.org>
2024-08-30 20:25:34 -0700

    using nested class instead of secondary node class

--- linkedlist/linkedlist.java
+++ linkedlist/linkedlist.java
@@ -1,5 +1,11 @@
+// (1) https://docs.oracle.com/javase/tutorial/java/javaOO/nested.html nested classes
+
 package linkedlist;
 
 public class linkedlist {
+
+    private static class Node { // see link (1) for nested class credit
+
+    }
     
 }
